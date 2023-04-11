b7fe6d38f1d95814b94f1a18b3b5a30663fb6ffa
unknown <raul.cremenescu@transiris.com>
2023-04-11 17:07:06 +0300

    first commit

---
+++ src/test/java/raul/projects/pricemonitoring/PriceMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package raul.projects.pricemonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceMonitoringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
